b8f051fee441b8b13a29c3eb2fd3ebcfcb9c6ec5
kirilaf78 <kirilaf78@gmail.com>
2026-04-30 22:33:39 +0200

    test file was formatted

--- src/test/java/videogamedb/VideoGameDbSimulation.java
+++ src/test/java/videogamedb/VideoGameDbSimulation.java
@@ -9,52 +9,47 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 public class VideoGameDbSimulation extends Simulation {
     private static FeederBuilder.Batchable<String> csvFeeder = csv("data/gameData.csv").circular();
     private HttpProtocolBuilder httpProtocol = http
-        .baseUrl("https://videogamedb.uk/api")
-        .acceptHeader("application/json")
-        .contentTypeHeader("application/json");
+            .baseUrl("https://videogamedb.uk/api")
+            .acceptHeader("application/json")
+            .contentTypeHeader("application/json");
 
-private ScenarioBuilder scn = scenario("Video Game DB Stress Test")
-        .feed(csvFeeder) // Each time a user enters the scenario, they take a new line from the CSV
-        .exec(authenticate)
-        .pause(2)
-        .exec(createNewGame)
-        .pause(2)
-        .exec(http("Verify API is alive")
-            .get("/videogame/1")
-            .check(status().is(200))
-        );
+    private ScenarioBuilder scn = scenario("Video Game DB Stress Test")
+            .feed(csvFeeder) // Each time a user enters the scenario, they take a new line from the CSV
+            .exec(authenticate)
+            .pause(2)
+            .exec(createNewGame)
+            .pause(2)
+            .exec(http("Verify API is alive")
+                    .get("/videogame/1")
+                    .check(status().is(200)));
     // authenticate video game db:
     private static ChainBuilder authenticate = exec(http("Authenticate")
-        .post("/authenticate")
-        .body(StringBody("{\"password\": \"admin\", \"username\": \"admin\"}"))
-        .check(jsonPath("$.token").saveAs("jwtToken"))
-    );
+            .post("/authenticate")
+            .body(StringBody("{\"password\": \"admin\", \"username\": \"admin\"}"))
+            .check(jsonPath("$.token").saveAs("jwtToken")));
 
-{
+    {
         setUp(
-            scn.injectOpen(
-                // 1. First run one scout
-                atOnceUsers(1),
-                // 2. Then pause for 5 seconds
-                nothingFor(5),
-                // 3. Then smoothly introduce 10 users over 30 seconds
-                rampUsers(10).during(30)
-            )
-        ).protocols(httpProtocol);
-     }
+                scn.injectOpen(
+                        // 1. First run one scout
+                        atOnceUsers(1),
+                        // 2. Then pause for 5 seconds
+                        nothingFor(5),
+                        // 3. Then smoothly introduce 10 users over 30 seconds
+                        rampUsers(10).during(30)))
+                .protocols(httpProtocol);
+    }
 
-private static ChainBuilder createNewGame = exec(http("Create new game - #{gameName}")
-        .post("/videogame")
-        .header("Authorization", "Bearer #{jwtToken}")
-        .body(StringBody(
-            "{" +
-            "  \"id\": #{gameId}, " +
-            "  \"category\": \"RPG\", " +
-            "  \"name\": \"#{gameName}\", " +
-            "  \"rating\": \"Mature\", " +
-            "  \"releaseDate\": \"2025-05-04\", " +
-            "  \"reviewScore\": 99" +
-            "}"
-        ))
-    );
+    private static ChainBuilder createNewGame = exec(http("Create new game - #{gameName}")
+            .post("/videogame")
+            .header("Authorization", "Bearer #{jwtToken}")
+            .body(StringBody(
+                    "{" +
+                            "  \"id\": #{gameId}, " +
+                            "  \"category\": \"RPG\", " +
+                            "  \"name\": \"#{gameName}\", " +
+                            "  \"rating\": \"Mature\", " +
+                            "  \"releaseDate\": \"2025-05-04\", " +
+                            "  \"reviewScore\": 99" +
+                            "}")));
 }
